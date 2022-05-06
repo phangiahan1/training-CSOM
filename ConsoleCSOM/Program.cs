7f66ae8e3a31b2dda61a530ec398e066ffd2dda2
Phan Gia Han <phanhan226@gmail.com>
2022-05-06 14:45:35 +0700

    commit all

--- ConsoleCSOM/Program.cs
+++ ConsoleCSOM/Program.cs
@@ -30,7 +30,7 @@ namespace ConsoleCSOM
                     ctx.Load(ctx.Web);
                     await ctx.ExecuteQueryAsync();
                     Console.WriteLine($"Site {ctx.Web.Title}");
-
+    
                     //[1.1] Using CSOM create a list name "CSOM Test"
                     //await CreateList(ctx, "CSOM Test", "using CSOM create a list");
 
@@ -49,9 +49,9 @@ namespace ConsoleCSOM
                     //await CreateSiteFieldTypeText(ctx, "about", "about", groupName);
                     //await CreateSiteFieldTypeTaxonomy(ctx, "city", "city", groupName);
 
-                    //[1.5] Create site content type "CSOM Test content type"
-                    //      => add this to "CSOM test"
-                    //      add fields "about" and "city" to this.
+                     //[1.5] Create site content type "CSOM Test content type"
+                     //      => add this to "CSOM test"
+                     //      add fields "about" and "city" to this.
                     string ContentTypeId = "0x0101009189AB5D3D2647B580F011DA2F356FB2";
                     string ContentTypeGroupName = "CSOM city projects Content Types";
                     //await CreateContentType(ctx, "CSOM Test content type", ContentTypeId, ContentTypeGroupName);
@@ -65,15 +65,46 @@ namespace ConsoleCSOM
                     //await SetDefaultContentType(ctx, "CSOM Test content type", "CSOM Test");
 
                     //[1.7] Create 5 list items to list with some value  in field "about" and "city"
-                    await CreateListItem(ctx, "CSOM Test", "phan gia han", "Ho Chi Minh");
+                    //await CreateListItem(ctx, "CSOM Test", "Pham Thi Bich Tram", "Ho Chi Minh");
+                    //await CreateListItem(ctx, "CSOM Test", "Trinh Gia Dinh", "Stockholm");
+                    //await CreateListItem(ctx, "CSOM Test", "Xa Thi Man", "");
 
                     //[1.8] Update site field "about" set default value for it to"about default" then create 2 new list items
-
+                    //await UpdateDefaultValueSiteFieldTypeText(ctx, "CSOM Test", "about", "about default");
+                    //await CreateListItem(ctx, "CSOM Test", null, "");
+                    //await CreateListItem(ctx, "CSOM Test", "Not null", "");
 
                     //[1.9] Update site field "city" set default value for it to"Ho Chi Minh" then create 2 new list items
+                    //await UpdateDefaultValueSiteFieldTypeTaxonomy(ctx, "CSOM Test", "city", "Ho Chi Minh");
+                    //await CreateListItem(ctx, "CSOM Test", "Cau 1.9", null);
+                    //await CreateListItem(ctx, "CSOM Test", null, null);
+
+                    //[2.1] Write CAML query to get list items where field “about” is not “about default”
+                    //////Eq Equals
+                    //////Neq Not equal
+                    //////Gt Greater than
+                    //////Geq Greater than or equal
+                    //////Lt  Lower than
+                    //////Leq Lower than or equal too
+                    //////IsNull Is null
+                    //////BeginsWith Begins with
+                    //////Contains Contains
+
+                    //await CAMLQueryWithWhere(ctx, "CSOM Test", "about", "Text", "Neq", "about default");
+
+                    //[2.2] Create List View by CSOM order item newest in top and only show list item where “city” field has value “Ho Chi Minh”,
+                    //View Fields: Id, Name, City, About
+
+                    //await CreateListViewWithOrderNewestAndWhereCityInHoChiMinh(ctx, "CSOM Test");
+
+
+                    //[2.3] Write function update list items in batch, try to update 2 items every time and update field “about” which have value
+                    //“about default” to “Update script”. (CAML)
+
+
+                    //[2.4] Create new field “author” type people in list “CSOM Test” then migrate all list items to set user admin to field “CSOM Test Author”
 
                 }
-
                 Console.WriteLine($"Press Any Key To Stop!");
                 Console.ReadKey();
             }
@@ -100,25 +131,6 @@ namespace ConsoleCSOM
                                    t => t.Name,
                                    t => t.Id);
             await Ctx.ExecuteQueryAsync();
-        }
-
-        private static async Task ExampleSetTaxonomyFieldValue(ListItem item, ClientContext ctx, string fieldName)
-        {
-            //var field = ctx.Web.Fields.GetByTitle(fieldName);
-
-            //ctx.Load(field);
-            //await ctx.ExecuteQueryAsync();
-
-            //var taxField = ctx.CastTo<TaxonomyField>(field);
-
-            //taxField.SetFieldValueByValue(item, new TaxonomyFieldValue()
-            //{
-            //    WssId = -1, // alway let it -1
-            //    Label = "city-han",
-            //    TermGuid = "ac5437a6-038e-4da7-8959-c9044ab38ce6"
-            //});
-            //item.Update();
-            //await ctx.ExecuteQueryAsync();
         }
 
         private static async Task CsomTermSetAsync(ClientContext ctx)
@@ -173,6 +185,7 @@ namespace ConsoleCSOM
             await ctx.ExecuteQueryAsync();
 }
 
+        //Exercise 1: 
         private static void GetTaxonomyFieldInfo(ClientContext clientContext, out Guid termStoreId, out Guid termSetId)
         {
             termStoreId = Guid.Empty;
@@ -189,7 +202,6 @@ namespace ConsoleCSOM
             termStoreId = termStore.Id;
             termSetId = termSets.FirstOrDefault()!.Id;
         }
-    
         private static async Task CreateList(ClientContext ctx, string listName, string description)
         {
             Console.WriteLine("Using CSOM create a list name: " + listName);
@@ -333,6 +345,23 @@ namespace ConsoleCSOM
             await ctx.ExecuteQueryAsync();
 
         }
+        public static void UpdateTaxonomyField(ClientContext ctx, List list, ListItem listItem, string fieldName, string fieldValue)
+        {
+            Console.WriteLine("UpdateTaxonomyField");
+            Field field = list.Fields.GetByInternalNameOrTitle(fieldName);
+            TaxonomyField txField = ctx.CastTo<TaxonomyField>(field);
+            TaxonomyFieldValue termValue = new TaxonomyFieldValue();
+            string[] term = fieldValue.Split('|');
+            termValue.Label = term[0];
+            termValue.TermGuid = term[1];
+            //termValue.Label = "Ho Chi Minh";
+            //termValue.TermGuid = "90dd8af9-e9f0-4f6e-ac57-68200c8ea34c";
+            termValue.WssId = -1;
+            txField.SetFieldValueByValue(listItem, termValue);
+            listItem.Update();
+            ctx.Load(listItem);
+            ctx.ExecuteQuery();
+        }
         private static async Task CreateListItem(ClientContext ctx, string listName, string about, string city)
         {
             List oList = ctx.Web.Lists.GetByTitle(listName);
@@ -340,14 +369,190 @@ namespace ConsoleCSOM
             ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
             ListItem oListItem = oList.AddItem(itemCreateInfo);
 
-
-            oListItem["about"] = about;            
-            oListItem["city"] = city;
-            //https://www.c-sharpcorner.com/uploadfile/anavijai/programmatically-set-value-to-the-taxonomy-field-in-sharepoint-2010/
+            if(about != null)
+            {
+                oListItem["about"] = about;
+            }
+            
+            string fieldValue;
+            if(city == "Ho Chi Minh")
+            {
+                fieldValue = "Ho Chi Minh|90dd8af9-e9f0-4f6e-ac57-68200c8ea34c";
+            } 
+            else if(city == "Stockholm")
+            {
+                fieldValue = "Stockholm|f50c5a60-1411-447d-81ca-4242f11d5380";
+            }
+            else
+            {
+                // If user input wrong set default city is Ho Chi Minh
+                fieldValue = "Ho Chi Minh|90dd8af9-e9f0-4f6e-ac57-68200c8ea34c";
+            }
+            UpdateTaxonomyField(ctx, oList, oListItem, "city", fieldValue);
 
             oListItem.Update();
 
             await ctx.ExecuteQueryAsync();
+        }
+        private static async Task UpdateDefaultValueSiteFieldTypeTextInList(ClientContext ctx, string listName, string fieldName, string defaultValueForField)
+        {
+            List list = ctx.Web.Lists.GetByTitle(listName);
+            Field field = list.Fields.GetByInternalNameOrTitle(fieldName);
+            field.DefaultValue = defaultValueForField;
+            field.Update();
+            await ctx.ExecuteQueryAsync();
+        }
+        private static async Task UpdateDefaultValueSiteFieldTypeText(ClientContext ctx, string listName, string fieldName, string defaultValueForField)
+        {
+            Web rootWeb = ctx.Site.RootWeb;
+            //List list = ctx.Web.Lists.GetByTitle(listName);
+            Field field = rootWeb.Fields.GetByInternalNameOrTitle(fieldName);
+            field.DefaultValue = defaultValueForField;
+            field.Update();
+            await ctx.ExecuteQueryAsync();
+        }
+        private static async Task UpdateDefaultValueSiteFieldTypeTaxonomyInList(ClientContext ctx, string listName, string fieldName, string fieldValue)
+        {
+            List olist = ctx.Web.Lists.GetByTitle(listName);
+            ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+            ListItem oListItem = olist.AddItem(itemCreateInfo);
+            Field field = olist.Fields.GetByInternalNameOrTitle(fieldName);
+            TaxonomyField txField = ctx.CastTo<TaxonomyField>(field);
+            TaxonomyFieldValue termValue = new TaxonomyFieldValue();
+            
+            if(fieldValue == "Ho Chi Minh")
+            {
+                termValue.Label = "Ho Chi Minh";
+                termValue.TermGuid = "90dd8af9-e9f0-4f6e-ac57-68200c8ea34c";
+                termValue.WssId = -1;
+            }
+            else
+            {
+                termValue.Label = "Stockholm";
+                termValue.TermGuid = "f50c5a60-1411-447d-81ca-4242f11d5380";
+                termValue.WssId = -1;
+            }
+            var validatedValue = txField.GetValidatedString(termValue);
+
+            await ctx.ExecuteQueryAsync();
+            txField.DefaultValue = validatedValue.Value;
+            txField.UserCreated = false;
+            txField.UpdateAndPushChanges(true);
+            oListItem.Update();
+            ctx.Load(oListItem);
+            await ctx.ExecuteQueryAsync();
+        }
+        private static async Task UpdateDefaultValueSiteFieldTypeTaxonomy(ClientContext ctx, string listName, string fieldName, string fieldValue)
+        {
+            Web rootWeb = ctx.Site.RootWeb;
+            //List olist = ctx.Web.Lists.GetByTitle(listName);
+            //ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+            //ListItem oListItem = olist.AddItem(itemCreateInfo);
+            Field field = rootWeb.Fields.GetByInternalNameOrTitle(fieldName);
+            TaxonomyField txField = ctx.CastTo<TaxonomyField>(field);
+            TaxonomyFieldValue termValue = new TaxonomyFieldValue();
+
+            if (fieldValue == "Ho Chi Minh")
+            {
+                termValue.Label = "Ho Chi Minh";
+                termValue.TermGuid = "90dd8af9-e9f0-4f6e-ac57-68200c8ea34c";
+                termValue.WssId = -1;
+            }
+            else
+            {
+                termValue.Label = "Stockholm";
+                termValue.TermGuid = "f50c5a60-1411-447d-81ca-4242f11d5380";
+                termValue.WssId = -1;
+            }
+            var validatedValue = txField.GetValidatedString(termValue);
+
+            await ctx.ExecuteQueryAsync();
+            txField.DefaultValue = validatedValue.Value;
+            txField.UserCreated = false;
+            txField.UpdateAndPushChanges(true);
+            rootWeb.Update();
+            ctx.Load(rootWeb);
+            await ctx.ExecuteQueryAsync();
+        }
+        
+        //Exercise 2:
+        private static async Task CAMLQueryWithWhere(ClientContext ctx, string listName, string fieldName, string fielsType, string Operators, string OperatorsValue)
+        {
+            List list = ctx.Web.Lists.GetByTitle(listName);
+
+            CamlQuery query = new CamlQuery();
+            query.ViewXml = "<View>"
+               + "<Query>"
+               + $"<Where><{Operators}><FieldRef Name='{fieldName}' /><Value Type='{fielsType}'>{OperatorsValue}</Value></{Operators}></Where>"
+               + "</Query>"
+               + "</View>";
+            // execute the query
+            ListItemCollection listItems = list.GetItems(query);
+            //foreach (ListItem item in listItems)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            ctx.Load(listItems);
+            await ctx.ExecuteQueryAsync();
+
+            foreach (ListItem oListItem in listItems)
+            {
+                TaxonomyFieldValue taxFieldValue = oListItem["city"] as TaxonomyFieldValue;
+                Console.WriteLine("about: {0}  - city: {1}",  oListItem["about"], taxFieldValue.Label);
+            }
+
+
+        }
+        private static async Task CreateListViewWithOrderNewestAndWhereCityInHoChiMinh(ClientContext ctx, string listName)
+        {
+            List targetList = ctx.Web.Lists.GetByTitle(listName);
+
+            ViewCollection viewCollection = targetList.Views;
+            ctx.Load(viewCollection);
+
+            ViewCreationInformation viewCreationInformation = new ViewCreationInformation();
+            viewCreationInformation.Title = "View With Order And City Is HCM";
+
+            // Specify type of the view. Below are the options
+
+            // 1. none - The type of the list view is not specified
+            // 2. html - Sspecifies an HTML list view type
+            // 3. grid - Specifies a datasheet list view type
+            // 4. calendar- Specifies a calendar list view type
+            // 5. recurrence - Specifies a list view type that displays recurring events
+            // 6. chart - Specifies a chart list view type
+            // 7. gantt - Specifies a Gantt chart list view type
+
+            viewCreationInformation.ViewTypeKind = ViewType.Grid;
+
+            // You can optionally specify row limit for the view
+            //viewCreationInformation.RowLimit = 10;
+
+            // You can optionally specify a query as mentioned below.
+            // Create one CAML query to filter list view and mention that query below
+            viewCreationInformation.Query = "<Where><Eq><FieldRef Name = 'city' /><Value Type = 'Taxonomy'>Ho Chi Minh</Value></Eq></Where>";
+            viewCreationInformation.Query = "<OrderBy><FieldRef Name='Created' Ascending='FALSE'/></OrderBy>";
+
+            // Add all the fields over here with comma separated value as mentioned below
+            // You can mention display name or internal name of the column
+            string CommaSeparateColumnNames = "about,city";
+            viewCreationInformation.ViewFields = CommaSeparateColumnNames.Split(',');
+
+            View listView = viewCollection.Add(viewCreationInformation);
+            ctx.ExecuteQuery();
+
+            // Code to update the display name for the view.
+            listView.Title = "View With Order And City Is HCM";
+
+            // You can optionally specify Aggregation: Field references for totals columns or calculated columns
+            //listView.Aggregations = "<FieldRef Name='Title' Type='COUNT'/>";
+
+            listView.Update();
+            await ctx.ExecuteQueryAsync();
+        }
+        private static async Task CAMLQueryUpdateMutiListItems(ClientContext ctx, string listName, string fieldName, string currentValue, string newValue)
+        {
+
         }
     }
 }
